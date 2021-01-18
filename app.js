414cb8f0c54a3e279d87ca999599e64ff67d701e
SOFIA <rusofiaru@gmail.com>
2021-01-18 22:26:31 +0200

    add static middleware

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 const productsRouter = require('./controllers/products')
 const availabilityRouter = require("./controllers/availability");
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 

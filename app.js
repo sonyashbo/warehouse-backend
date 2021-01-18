948303093e826767257b072e61fa276b226a09d9
SOFIA <rusofiaru@gmail.com>
2021-01-18 23:50:21 +0200

    add middleware unknown endpoint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const middleware = require('./utils/middleware')
 const productsRouter = require('./controllers/products')
 const availabilityRouter = require("./controllers/availability")
+const path = require("path");
 
 app.use(cors())
 app.use(express.static('build'))
@@ -13,7 +14,12 @@ app.use(middleware.requestLogger)
 app.use('/api/products', productsRouter)
 app.use('/api/availability', availabilityRouter)
 
-app.use(middleware.unknownEndpoint)
+app.get('/*', (req, res) => {
+    let url = path.join(__dirname, '../build', 'index.html')
+    res.sendFile(url)
+});
+
+//app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
